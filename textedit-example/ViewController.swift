f33657909a8ef485ed1ebcf1bb4b12f1b9f1172f
Aleksandr Zhuravlev <geekbrains.zhuravlev@yandex.ru>
2018-08-28 12:36:47 +0300

    Initial Commit

---
+++ textedit-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  textedit-example
+//
+//  Created by Aleksandr Zhuravlev on 28/08/2018.
+//  Copyright © 2018 Aleksandr Zhuravlev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
